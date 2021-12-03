719306261f9d0fa99af5c3d539a2e05cc4fc191f
CHEIKHOUN <94837299+CHEIKHOUN@users.noreply.github.com>
2021-12-03 06:41:49 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>  s
+<html>
+<head>
+	  <meta charset="utf-8">
+	  <title> mon premier en javascript</title>
+</head>
+<body>
+	  <h1>je voulais JS #0 </h1>
+
+	  <script src="" ="general/app.js"></script>
+	  <script>
+		console.log(prompt("comment va tu"))
+	  </script>
+
+
+</body>
+</html>
+
+
+
+
